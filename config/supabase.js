1601855868122ca4f2b8ff236e2da6331be8a286
Mostafa Ismail Alanani <asmaylmr117@gmail.com>
2025-03-15 02:37:25 +0200

    supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,9 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  'https://xfuggyswtwfqvtpnwtjz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmdWdneXN3dHdmcXZ0cG53dGp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4MDUzODUsImV4cCI6MjA1NzM4MTM4NX0.LWU2YUnYuHnzvLXJYRAyaZHOR2vDk3-WD3RbToWvniM'
 );
 
 module.exports = supabase;
